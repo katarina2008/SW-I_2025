001482162347f399c2482f6a404349673f3489c2
katarina2008 <160730590+katarina2008@users.noreply.github.com>
2025-02-27 10:59:52 -0300

    Add files via upload

---
+++ Aula03/processa01.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_GET['cxnome'];
+    $idade = $_GET['cxidade'];
+    echo "Nome digitado: $nome <br>";
+    echo "Idade digitada: $idade anos de vida! <br>";
+
+?>
---
+++ Aula03/processa_exe01.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_POST['cxnome'];
+    $mensagem = $_POST['cxmensagem'];
+    echo " Obrigada por sua mensagem $nome ! <br>";
+    echo "Sua mensagem foi: $mensagem <br>";
+
+?>
