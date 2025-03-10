304accfe28e5d91e00ce0ee181111efcb9ec4bc4
katarina2008 <katarina.vitoria100@gmail.com>
2025-03-10 08:01:22 -0300

    Finalizando

---
+++ Aula_04/Exercício05/form05.php
@@ -0,0 +1,17 @@
+<?php
+    $usuário = $_POST['cxusuário'];
+    $cor = $_POST['cxcor'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <h1>Cor</h1>
+</head>
+<body  style="background-color: <?php echo "$cor" ?>; margin: 20%;">
+    
+  
+</body>
+</html>
